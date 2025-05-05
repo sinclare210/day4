a0ece6204d6300a5c33cd6694006618a4ee6c6b9
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-05 23:01:46 +0100

    Done with scripting  i guess

--- script/AuctionHouse.s.sol
+++ script/AuctionHouse.s.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import {AuctionHouse} from "../src/AuctionHouse.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract AuctionHouseScript is Script {
+    AuctionHouse public auctionHouse;
+
+    function run() public{
+        vm.startBroadcast();
+        auctionHouse = new AuctionHouse("Vintage Clock", 3600);
+        vm.stopBroadcast();
+    }
+
+}
